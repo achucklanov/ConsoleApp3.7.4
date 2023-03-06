20b04a125e9fdd1755cbf8008770389df14ae124
AndreyChuck <a.chucklanov@yandex.ru>
2023-03-06 22:01:19 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp3.7.4/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp3._7._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name:  ");
+            string Name = Console.ReadLine();
+
+            Console.Write("Enter your age:  ");
+            var Age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your name is {0}, and your age is {1}", Name, Age);
+
+            Console.Write("Enter your birthdate: ");
+            var Bd = Console.ReadLine();
+            Console.WriteLine("Your birthdate is {0}", Bd);
+
+            Console.ReadKey();
+        }
+    }
+}
